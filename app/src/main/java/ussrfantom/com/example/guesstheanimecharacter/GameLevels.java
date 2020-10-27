d2e1e256a72ab81f738fd8860cab70efbd4e738a
Ussrfantom <dimkakrohin@mail.ru>
2020-10-27 23:21:30 +0300

    add GameLevels

--- app/src/main/java/ussrfantom/com/example/guesstheanimecharacter/GameLevels.java
+++ app/src/main/java/ussrfantom/com/example/guesstheanimecharacter/GameLevels.java
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.util.ArrayList;
 
@@ -14,19 +16,27 @@ public class GameLevels extends AppCompatActivity {
 
     private RecyclerView recyclerViewLevels;
     private ArrayList <Levels> levels = new ArrayList<>();
+    public static int numberOfPositionsSolved; //колличество решенных уровней
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_levels);
         recyclerViewLevels = findViewById(R.id.recyclerViewLevels);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        levels.add(new Levels("https://you-anime.ru/anime-images/characters/132959.jpg"));
-        levels.add(new Levels("https://you-anime.ru/anime-images/characters/132959.jpg"));
-        levels.add(new Levels("https://you-anime.ru/anime-images/characters/132959.jpg"));
-        levels.add(new Levels("https://you-anime.ru/anime-images/characters/132959.jpg"));
-        levels.add(new Levels("https://you-anime.ru/anime-images/characters/132959.jpg"));
-        levels.add(new Levels("https://you-anime.ru/anime-images/characters/132959.jpg"));
+        //заполнение массива
+        for (int i = 0; i <=50; i++){
+            if(i < numberOfPositionsSolved){
+                levels.add(new Levels("https://a.radikal.ru/a35/2010/68/d2934cfdd690.jpg"));
+            }else{
+                levels.add(new Levels("https://c.radikal.ru/c29/2010/db/7aeff8e7ddfa.jpg"));
+            }
+        }
+
+
+
         LevelsAdapter adapter = new LevelsAdapter(levels);
         recyclerViewLevels.setLayoutManager(new GridLayoutManager(this, 4));
         recyclerViewLevels.setAdapter(adapter);
--- app/src/main/java/ussrfantom/com/example/guesstheanimecharacter/MainActivity.java
+++ app/src/main/java/ussrfantom/com/example/guesstheanimecharacter/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+        GameLevels.numberOfPositionsSolved = 1;
     }
 
 }
